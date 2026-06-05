14a8be37470149515a15b38ec637e1d4f867c751
Rodrigo Pereira <2025113163@ufp.edu.pt>
2026-06-05 19:51:47 +0100

    base de dados upd

--- db.js
+++ db.js
@@ -9,18 +9,18 @@ const db = new sqlite3.Database('./publicacoes.db');
 db.serialize(() => {
 
     /**
-     * Cria a tabela "publicacoes" caso ainda não exista.
-     * 
-     * Estrutura da tabela:
-     * - id       : Identificador interno automático
-     * - ogid     : ID original da publicação
-     * - secao    : Categoria/Secção da publicação
-     * - autores  : Nome(s) dos autores
-     * - ano      : Ano da publicação
-     * - titulo   : Título da publicação
-     * - lingua   : Língua da publicação
-     * - link     : URL da publicação
-     */
+    * Cria a tabela "publicacoes" caso ainda não exista.
+    * 
+    * Estrutura da tabela:
+    * - id       : Identificador interno automático
+    * - ogid     : ID original da publicação
+    * - secao    : Categoria/Secção da publicação
+    * - autores  : Nome(s) dos autores
+    * - ano      : Ano da publicação
+    * - titulo   : Título da publicação
+    * - lingua   : Língua da publicação
+    * - link     : URL da publicação
+    */
     db.run(`
         CREATE TABLE IF NOT EXISTS publicacoes (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,6 +31,26 @@ db.serialize(() => {
             titulo TEXT,
             lingua TEXT,
             link TEXT
+        )
+    `);
+
+    /**
+    * Cria a tabela "utilizadores" caso ainda não exista.
+    * 
+    * Estrutura da tabela:
+    * - id             : Identificador interno automático
+    * - username       : Username do perfil (é unico)
+    * - email          : Email do perfil (é unico)
+    * - password_hash  : Password em hash (bcrypt)
+    * - created_at     : Data da criação
+    */
+    db.run(`
+        CREATE TABLE IF NOT EXISTS utilizadores (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE,
+            password_hash TEXT NOT NULL,
+            created_at TEXT DEFAULT (datetime('now'))
         )
     `);
 });
